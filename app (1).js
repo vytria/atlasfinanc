30a40036c648752b3cfba310a3aa91bcabc9c99d
vytria <victorzumopinheiro@gmail.com>
2026-01-12 12:31:56 -0300

    Add files via upload

---
+++ app (1).js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Atlas – Controle Financeiro</title>
+<link rel="stylesheet" href="style.css">
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+
+<header>
+<h2>Atlas – Controle Financeiro</h2>
+<div class="cards">
+  <div class="card"><span id="receita"></span><small>Receita</small></div>
+  <div class="card"><span id="despesa"></span><small>Despesa</small></div>
+  <div class="card"><span id="saldo"></span><small>Saldo</small></div>
+  <div class="card"><span id="percentual"></span><small>% Comprometido</small></div>
+</div>
+</header>
+  
+
+<main>
+<select id="mes"></select>
+<canvas id="chart"></canvas>
+<table id="table"></table>
+</main>
+
+<script src="data.js"></script>
+<script src="app.js"></script>
+
+</body>
+</html>
